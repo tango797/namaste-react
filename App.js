7aa2616cabe05f58a4d53ff895bfe1d1aa95c8c4
Tango797 <tewatia35@gmail.com>
2023-11-02 22:08:01 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,5 @@
+const heading = React.createElement("h1",{id:"heading",xyz:"xyz"},[React.createElement("h1",{id:"heading",xyz:"xyz"},"hello how are you")]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(heading)
